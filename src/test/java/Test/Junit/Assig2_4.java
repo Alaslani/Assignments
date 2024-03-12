0af908c4a8325da99547f8c41cefe223898eb20e
Ahmed Alaslani <Ahmed@Ahmeds-MacBook-Pro.local>
2024-03-12 20:42:01 +0300

    Assig2_4

--- src/test/java/Test/Junit/Assig2_4.java
+++ src/test/java/Test/Junit/Assig2_4.java
@@ -8,6 +8,14 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+/*
+Go to URL: http://demo.guru99.com/test/drag_drop.html
+Drag and drop the BANK button to the Account section in DEBIT SIDE
+Drag and drop the SALES button to the Account section in CREDIT SIDE
+Drag and drop the 5000 button to the Amount section in DEBIT SIDE
+Drag and drop the second 5000 button to the Amount section in CREDIT SIDE
+Verify the visibility of Perfect text.
+*/
 public class Assig2_4 extends Tests {
 
     @Test
